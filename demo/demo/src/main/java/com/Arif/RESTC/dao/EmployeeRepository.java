c04610be03b8b7a022db7bfa4b2b860ddb618ce7
arifur946954 <arif.bd08060@gmail.com>
2023-10-13 19:34:20 +0600

    jpa rest

--- demo/demo/src/main/java/com/Arif/RESTC/dao/EmployeeRepository.java
+++ demo/demo/src/main/java/com/Arif/RESTC/dao/EmployeeRepository.java
@@ -1,2 +1,9 @@
-package com.Arif.RESTC.dao;public interface EmployeeRepository {
+package com.Arif.RESTC.dao;
+
+import com.Arif.RESTC.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path ="teacher")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
